e36f7560f3b3c3e086d3c9ea51cd0c2732464bc7
tmharkins <tmharkins@gmail.com>
2016-01-14 07:15:56 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,14 @@
-# code your solution here
+def power_digit_sum(x,n)
+	digit = x
+	power = n
+	sum = digit ** power
+	sum_string = sum.to_s
+	len = sum_string.length
+	total = 0
+	until len == 0
+		sum_int = sum_string[len-1].to_i
+		total = total + sum_int
+		len -= 1
+	end
+	return total
+end
